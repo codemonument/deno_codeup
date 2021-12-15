daa674b0e77662710af796562d74fe4e06e3de20
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-12-15 17:54:22 +0100

    use new startKia function

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { emptyDir, ensureDir } from "https://deno.land/std@0.117.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.117.0/path/mod.ts";
 import * as log from "https://deno.land/std@0.117.0/log/mod.ts";
-import { Kia } from "https://deno.land/x/kia@v0.1.0/kia.ts";
 import { cleanupUserTempDirs } from "./src/cleanup-user-temp-dirs.ts";
 import { downloadVSCodeZip } from "./src/download-vscode-zip.ts";
+import { startKia } from "./src/utils/start-kia.ts";
 
 /**
  * IMPORTANT: This script assumes to be started inside an extracted vscode installation
@@ -13,13 +13,11 @@ const logger = log.getLogger();
 try {
   logger.info("Updating vscode...");
 
-  const kiaCleanup = new Kia("Cleanup user temp dirs...");
-  await kiaCleanup.start();
+  const kiaCleanup = await startKia("Cleanup user temp dirs...");
   await cleanupUserTempDirs();
   await kiaCleanup.succeed("Cleaned temp files in user Data");
 
-  const kiaZipDownload = new Kia("Downloading vscode zip");
-  await kiaZipDownload.start();
+  const kiaZipDownload = await startKia("Downloading vscode zip");
   await downloadVSCodeZip();
   await kiaZipDownload.succeed(`Downloaded VSCode Zip`);
 
