a6d7e576fbd36ceb89f44984595e5e3aef35a378
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-12-17 17:33:07 +0100

    update zip module to 1.2.2

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { cleanupUserTempDirs } from "./src/cleanup-user-temp-dirs.ts";
 import { downloadVSCodeZip } from "./src/download-vscode-zip.ts";
 import { startKia } from "./src/utils/start-kia.ts";
 import { cleanFolder } from "./src/clean-folder.ts";
-import { unZipFromFile } from "https://deno.land/x/zip@v1.1.0/mod.ts";
+import { decompress } from "https://deno.land/x/zip@v1.2.2/mod.ts";
 
 /**
  * IMPORTANT: This script assumes to be started inside an extracted vscode installation
@@ -19,8 +19,8 @@ try {
   });
 
   const kiaUnzip = await startKia(`Unzip ${updateZip}`);
-  const result = await unZipFromFile(updateZip, ".");
-  if (result.valueOf() === false) throw new Error(`Zip Extraction failed!`);
+  const result = await decompress(updateZip, ".");
+  if (result === false) throw new Error(`Zip Extraction failed!`);
   await kiaUnzip.succeed(`Unzipped ${updateZip}`);
 
   const kiaZipDelete = await startKia(`Remove ${updateZip}`);
