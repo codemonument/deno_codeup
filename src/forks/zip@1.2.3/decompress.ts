06f50a9b3a70f591f85c8bcfc40441607a627b96
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-30 17:38:14 +0200

    add ability to force extraction of zip file

--- src/forks/zip@1.2.3/decompress.ts
+++ src/forks/zip@1.2.3/decompress.ts
@@ -1,10 +1,18 @@
 import { exists, join } from "./deps.ts";
 import { getFileNameFromPath } from "./utils.ts";
+interface DecompressOptions {
+    /**
+     * Overwrites files when they're already present at the target location
+     * (Translates to PowerShell Expand-Archive -Force )
+     */
+    overwrite?: boolean;
+    includeFileName?: boolean;
+  }
 
 export const decompress = async (
   filePath: string,
   destinationPath: string | null = "./",
-  options: any = {},
+  options: DecompressOptions = {},
 ): Promise<string | false> => {
   // check if the zip file is exist
   if (!await exists(filePath)) {
@@ -23,19 +31,30 @@ export const decompress = async (
     : destinationPath;
 
   // return the unzipped file path or false whene the unzipping Process failed
-  return await decompressProcess(filePath, fullDestinationPath)
+  return await decompressProcess(filePath, fullDestinationPath, options.override ?? false)
     ? fullDestinationPath
     : false;
 };
+
+/**
+ * PowerShell Expand-Archive Docs: 
+ * https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/expand-archive?view=powershell-7.2
+ * @param zipSourcePath 
+ * @param destinationPath 
+ * @param force 
+ * @returns 
+ */
 const decompressProcess = async (
   zipSourcePath: string,
   destinationPath: string,
+  force: boolean
 ): Promise<boolean> => {
   const unzipCommandProcess = Deno.run({
     cmd: Deno.build.os === "windows"
       ? [
         "PowerShell",
         "Expand-Archive",
+        (force) ? "-Force": "",
         "-Path",
         `"${zipSourcePath}"`,
         "-DestinationPath",
