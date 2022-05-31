e0a60c51da1f595fcb4fe65861ecaa2302f02ec7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-31 12:35:42 +0200

    implement tests for isVSCodeFolder

--- src/utils/is-vscode-folder.test.ts
+++ src/utils/is-vscode-folder.test.ts
@@ -1,8 +1,20 @@
 import { assertEquals, describe, it } from "../deps/_testing.std.ts";
 import { isVSCodeFolder } from "./is-vscode-folder.ts";
+import { ensureFile } from "../deps/_fs.std.ts";
 
 describe(`isVSCodeFolder`, () => {
-  it(`should return false for ./playground `, async () => {
+  it(`should return false for existing folder ./dist `, async () => {
     assertEquals(await isVSCodeFolder("./dist"), false);
+  });
+
+  it(`should return false for illegal folder ./bob `, async () => {
+    assertEquals(await isVSCodeFolder("./bob"), false);
+  });
+
+  it(`should return true for folder with fake Code.exe`, async () => {
+    const fakeExe = `./playground/Code.exe`;
+    await ensureFile(fakeExe);
+    assertEquals(await isVSCodeFolder("./playground"), true);
+    await Deno.remove(fakeExe);
   });
 });
