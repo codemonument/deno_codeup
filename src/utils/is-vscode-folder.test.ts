889c130a9d29b7c5e079e57575b0ec60c822021b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-31 08:59:50 +0200

    add util function: is-vscode-folder

---
+++ src/utils/is-vscode-folder.test.ts
@@ -0,0 +1,4 @@
+import { describe } from "../deps/_testing.std.ts";
+
+describe(`is-vscode-folder`, () => {
+});
---
+++ src/utils/is-vscode-folder.ts
@@ -0,0 +1,6 @@
+/**
+ * @param path The path to check whether it is a vscode install folder or not
+ */
+export async function isVscodeFolder(path: string): Promise<boolean> {
+  return false;
+}
