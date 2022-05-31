9c6c8a08a169296a7a934083a17e23eb6d66dedc
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-31 15:27:14 +0200

    add doc comments to CliArgs interface

--- src/utils/cli-args.ts
+++ src/utils/cli-args.ts
@@ -1,4 +1,16 @@
 export interface CliArgs {
+  /**
+   * If true, this cli does not override any files hile extracting the update zip.
+   */
   safeExtract: boolean;
+
+  /**
+   * If true, allows installation to installLocation, when installLocation is empty
+   */
+  allowInstall: boolean;
+
+  /**
+   * The path of the vscode installation which should be updated
+   */
   installLocation?: string;
 }
