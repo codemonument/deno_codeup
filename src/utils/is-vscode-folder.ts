f5907a9e7f16d28de364406f01a0a8e7adb32845
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-31 12:45:41 +0200

    add method to check vscode folder synchronously

--- src/utils/is-vscode-folder.ts
+++ src/utils/is-vscode-folder.ts
@@ -47,3 +47,50 @@ export async function isVSCodeFolder(
 
   return true;
 }
+
+/**
+ * @param path The path to check whether it is a vscode install folder or not.
+ */
+export function isVSCodeFolderSync(
+  installDirPath: string | undefined,
+): boolean {
+  try {
+    if (installDirPath === undefined) {
+      log.debug(
+        `[isVSCodeFolder] Got undefined as path to test. Returning false`,
+      );
+      return false;
+    }
+
+    const absPath = path.resolve(installDirPath);
+    const installDirStats = Deno.statSync(absPath);
+
+    // Check if install dir path is a directory
+    if (!installDirStats.isDirectory) {
+      log.debug(
+        `[isVscodeFolder] installDir ${absPath} is not a directory`,
+      );
+      return false;
+    }
+
+    // check if install dir contains a Code.exe
+    const vscodeExePath = path.join(absPath, "Code.exe");
+    const hasVscodeExe = Deno.statSync(vscodeExePath);
+    if (!hasVscodeExe.isFile) {
+      log.debug(
+        `[isVscodeFolder] vscodeExe ${vscodeExePath} is not a file`,
+      );
+      return false;
+    }
+  } catch (error: unknown) {
+    if (error instanceof Deno.errors.NotFound) {
+      log.debug(`[isVscodeFolder] vscode folder or Code.exe was not found`);
+      return false;
+    }
+
+    log.error(error);
+    return false;
+  }
+
+  return true;
+}
