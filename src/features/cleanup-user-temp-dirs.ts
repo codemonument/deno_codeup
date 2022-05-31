409e17efd4741b76fdf2fafb3d56614f4a9f3b56
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-31 15:33:48 +0200

    update cleanup user temp dirs to use VSCodeInstallLocation

--- src/features/cleanup-user-temp-dirs.ts
+++ src/features/cleanup-user-temp-dirs.ts
@@ -1,9 +1,12 @@
 import { emptyDir } from "https://deno.land/std@0.117.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.117.0/path/mod.ts";
 import { startKia } from "../utils/start-kia.ts";
+import { VSCodeInstallLocation } from "../types/vscode-install-location.ts";
 
 // remove temp files from portable user data
-export async function cleanupUserTempDirs(cwd = Deno.cwd()): Promise<void[]> {
+export async function cleanupUserTempDirs(
+  vscodeInstall: VSCodeInstallLocation,
+): Promise<void[]> {
   const kiaCleanup = await startKia("Cleanup user temp dirs...");
 
   const tempFolderRemovalPromises = [
@@ -13,7 +16,12 @@ export async function cleanupUserTempDirs(cwd = Deno.cwd()): Promise<void[]> {
     "GPUCache",
     "logs",
   ].map((tempFolderName) => {
-    const path = join(cwd, "data", "user-data", tempFolderName);
+    const path = join(
+      vscodeInstall.location,
+      "data",
+      "user-data",
+      tempFolderName,
+    );
     return emptyDir(path);
   });
 
