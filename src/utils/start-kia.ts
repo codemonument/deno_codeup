5532ebe3fa070685f9e5b5be06109fad0aa94b6f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 13:41:58 +0200

    add divider in kia to avoid text mushing together with the kia prompt

--- src/utils/start-kia.ts
+++ src/utils/start-kia.ts
@@ -1,7 +1,7 @@
 import { Kia } from "../deps/_kia.ts";
 
 export async function startKia(text: string): Promise<Kia> {
-  const kia = new Kia(`${text}`);
+  const kia = new Kia(`${text} :: `);
   await kia.start();
   return kia;
 }
