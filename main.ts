f57ec67bfd9fd0ad9d49db4a8036efa381e62508
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 11:17:29 +0200

    add nice prompt in main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,11 @@
-import { cleanupUserTempDirs } from "./src/features/cleanup-user-temp-dirs.ts";
-import { downloadVSCodeZip } from "./src/features/download-vscode-zip.ts";
-import { startKia } from "./src/utils/start-kia.ts";
-import { cleanFolder } from "./src/features/clean-folder.ts";
-import { decompress } from "./src/forks/zip@1.2.3/mod.ts";
 import { VERSION } from "./VERSION.ts";
-import { runCli } from "./src/utils/runCli.ts";
 import { log } from "./src/deps/_log.std.ts";
-import { join } from "./src/deps/_path.std.ts";
-import { chooseValidVSCodeInstall } from "./src/features/choose-valid-vscode-install.ts";
-
+import { runCli } from "./src/cli-definition/runCli.ts";
 try {
   log.info(
-    `Running codeup, the portable-vscode-updater \n Version ${VERSION} \n`,
+    `Running codeup, the portable-vscode-updater
+     Version: ${VERSION} \n`,
   );
-
   // Main Command
   await runCli(Deno.args);
 } catch (error) {
