4c78ff99769f55262b9a6e099b13ca1bf8c4c736
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 19:18:09 +0200

    format code in main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { VERSION } from "./VERSION.ts";
 import { log } from "./src/deps/_log.std.ts";
 import { runCli } from "./src/cli-definition/runCli.ts";
+
 try {
   log.info(
     `Running codeup, the portable-vscode-updater
