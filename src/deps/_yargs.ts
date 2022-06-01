8b1951b6c1cbd803bf18719f0be1a8e0a74d6ae6
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 10:15:22 +0200

    export yargs default

--- src/deps/_yargs.ts
+++ src/deps/_yargs.ts
@@ -1,2 +1,6 @@
 export * from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
+export {
+  default,
+  default as yargs,
+} from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
 export * from "https://deno.land/x/yargs@v17.5.1-deno/deno-types.ts";
