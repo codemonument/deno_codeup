6e4867250f1ec41db24172bb5e9d0f1dd599019d
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 09:20:37 +0200

    add implementation of help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,18 @@
+import { log } from "../deps/_log.std.ts";
+
+type ParserWithHelpFunction = { help: (...cmdPath: string[]) => void };
+
 /**
  * Implements the help command for codeup cli
+ *
+ * @param parser This is the args parser instance
  */
-export function help() {
+export function help<T extends ParserWithHelpFunction>(
+  parser: T,
+  cmdPath: readonly string[] = [],
+) {
+  if (!cmdPath.length) {
+    log.info(`\nUSAGE: => TODO: Output usage information!\n`);
+  }
+  log.info(`\n${parser.help(...cmdPath)}\n`);
 }
