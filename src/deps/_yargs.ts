f6a679c2811f99785fccf35e1b29ba501cdd9651
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 10:27:11 +0200

    Make YargsInstace type useable in my code

--- src/deps/_yargs.ts
+++ src/deps/_yargs.ts
@@ -4,3 +4,9 @@ export {
   default as yargs,
 } from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
 export * from "https://deno.land/x/yargs@v17.5.1-deno/deno-types.ts";
+
+/**
+ * Make YargsInstace type useable in my code
+ */
+import yargs from "https://deno.land/x/yargs@v17.5.1-deno/deno.ts";
+export type YargsInstance = ReturnType<typeof yargs>;
