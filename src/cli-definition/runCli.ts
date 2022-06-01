72ae2b42db0683b0ee493997ba3723a439c6a7ca
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 11:55:43 +0200

    inline cli args parser into runCli file and delete unused code

--- src/cli-definition/runCli.ts
+++ src/cli-definition/runCli.ts
@@ -1,43 +1,27 @@
-import { MAIN_COMMAND, PARSE_FAILURE } from "../deps/_args.ts";
 import { log } from "../deps/_log.std.ts";
-import { parser } from "./cli-args.parser.ts";
-import { help } from "../commands/help.ts";
-import { install } from "../commands/install.ts";
-import { update } from "../commands/update.ts";
+import { yargs } from "../deps/_yargs.ts";
+import { VERSION } from "../../VERSION.ts";
+import { updateYargsCommand } from "../commands/update.ts";
+import { installYargsCommand } from "../commands/install.ts";
+
+/**
+ * The yargs parser instance.
+ * Exported for easier unit testing
+ */
+export const parser = yargs()
+  .command(
+    "$0",
+    "Installs or Updates portable vscode installations",
+    // () => console.log("default command was called!"),
+  )
+  .command(installYargsCommand)
+  .command(updateYargsCommand)
+  .help()
+  .version(VERSION);
 
 /**
  * @param args should normally contain Deno.args, but can also contain mocked data for testing
  */
 export async function runCli(args: string[]): Promise<void> {
-  const res = parser.parse(args);
-
-  switch (res.tag) {
-    case PARSE_FAILURE:
-      console.error(res?.error?.toString());
-      Deno.exit(1);
-      break;
-    case MAIN_COMMAND: {
-      const remaining = res.remaining().rawValues();
-      if (remaining.length) {
-        console.error(`Invalid subcommand: ${remaining[0]}`);
-      } else {
-        console.error("Missing subcommand");
-      }
-
-      // show usage when main exe was called without subcommands
-      help(parser);
-      Deno.exit();
-      break;
-    }
-    case "install":
-      // await install(res.value.value);
-      break;
-    case "update":
-      await update(res.value.value);
-      break;
-    case "help":
-    default:
-      help(parser, res.remaining().rawValues());
-      Deno.exit();
-  }
+  const res = await parser.parseAsync(args).argv;
 }
