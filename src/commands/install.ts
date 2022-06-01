3472fa00cef7f35c4bd42c58121a1f52b2ff1837
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 10:48:37 +0200

    add yargs command module exports for install.ts

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -5,3 +5,13 @@ import { InstallArgs } from "./installArgs.type.ts";
  */
 export async function install({}: InstallArgs) {
 }
+
+/**
+ * Export yargs command module
+ * See: https://github.com/yargs/yargs/blob/main/docs/advanced.md#providing-a-command-module
+ */
+
+export const command = "install";
+export const handler = install;
+export const describe =
+  "Installs a fresh copy of portable vscode into an empty folder";
