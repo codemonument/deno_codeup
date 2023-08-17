5548e64d6b3e11c4aedc2ba5c17b118b63eba5fa
Benjamin Jesuiter <benjamin.jesuiter.extern@atruvia.de>
2023-08-17 15:30:02 +0200

    add warning to main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,15 @@ import { VERSION } from "./VERSION.ts";
 import { log } from "./src/deps/std.ts";
 import { runCli } from "./src/cli-definition/runCli.ts";
 
+/**
+ * IMPORTANT: DO NOT RENAME THIS FILE!
+ * It is used for installing codeup, so it might produce problems with older installations!
+ */
+
 try {
   log.info(
     `Running codeup, the portable-vscode-updater
-     Version: ${VERSION} \n`,
+     Version: ${VERSION} \n`
   );
   // Main Command
   await runCli(Deno.args);
