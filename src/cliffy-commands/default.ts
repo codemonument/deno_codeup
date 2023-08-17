52ead9d1b9fc28100fe96cadc892bd8be393009b
Benjamin Jesuiter <benjamin.jesuiter.extern@atruvia.de>
2023-08-17 15:36:22 +0200

    add description to default command

---
+++ src/cliffy-commands/default.ts
@@ -0,0 +1,8 @@
+/**
+ * This is the default command function.
+ * Will be run when no explicit command is given.
+ *
+ * It contains an interactive process for detecting and upgrading existing portable vscode installations.
+ */
+
+export const dummy = "";
