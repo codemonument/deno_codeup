05c222f2e1d33ba03749efda16a61ca9279204e9
Benjamin Jesuiter <Benjamin.Jesuiter.Extern@atruvia.de>
2023-09-26 22:14:24 +0200

    cleanup std dependencies

--- src/deps/std.ts
+++ src/deps/std.ts
@@ -1,4 +1,3 @@
-export * as log from "https://deno.land/std@0.198.0/log/mod.ts";
-export * from "https://deno.land/std@0.198.0/fs/mod.ts";
-export * from "https://deno.land/std@0.198.0/path/mod.ts";
-export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.202.0/log/mod.ts";
+export * from "https://deno.land/std@0.202.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.202.0/path/mod.ts";
